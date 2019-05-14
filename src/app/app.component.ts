24f313852cffcb82da7eb4915f6361a85b960e89
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-14 08:24:15 -0400

    defined the table using the cdk table from material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface AccountsTypes{
+	id: string;
+	name: string;
+	available_cash: string;
+	today_change: string;
+}
+
+const ACCOUNTS_DATA: AccountsTypes[] = [
+	{
+		id: '0029',
+		name: 'AAA - 0029',
+		available_cash: '39,160,334.42',
+		today_change: '-0.07% /$31, 435,87'
+	}
+	
+]
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +25,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'morgan-stanley';
+hideAccountId = true;
+displayedColumns: string[] = ['name',  'available_cash'];
+dataSource = ACCOUNTS_DATA;
+
 }
