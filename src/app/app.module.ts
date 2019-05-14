50416bcfa82c72e7c36e2bdcb88c3f707a4b186f
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-14 07:15:25 -0400

    configured the AppModule with the cdk and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import {CdkTableModule} from '@angular/cdk/table'; 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+const ComponentDevKit_Modules = [
+	CdkTableModule
+];
+
+const Material_Modules = [
+       MatButtonModule,
+       MatCheckboxModule
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
