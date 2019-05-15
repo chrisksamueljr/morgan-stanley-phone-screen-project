3142ed3441c34658cd51659d3ce4bd274c4d634c
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-15 14:27:50 -0400

    configured the sorting using the matSort Directive able to trigger a sort but there is a bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,53 @@
-import { Component } from '@angular/core';
-import {Sort} from '@angular/material';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import {Sort, MatSort, MatTableDataSource} from '@angular/material';
 
-export interface AccountsTypes{
-	id: string;
+export interface AccountsTypes { 
+	identity: number;
 	name: string;
 	available_cash: string;
 	today_change: string;
 	positive: boolean;
 }
 
+
 const ACCOUNTS_DATA: AccountsTypes[] = [
 	{
-		id: '0029',
+		identity: 0,
 		name: 'AAA - 0029',
 		available_cash: '$39,160,334.42',
 		today_change: '-0.07% /$31, 435,87',
 		positive: false
 	},
 	{
-		id: '0146',
+		identity: 1 ,
 		name: 'IRA - 0146',
 		available_cash: '$15,884,302.39',
 		today_change: '+0.03% /$7,430.83',
 		positive: true
 	},
 	{
-		id: '1812',
+		identity: 2 ,
 		name: 'AAA - 1812',
 		available_cash: '$2,010,926.10',
 		today_change: '+0.21% /$38,881.63',
 		positive: true
 	},
 	{
-		id: '2019',
+		identity: 3 ,
 		name: 'REG - 2019',
 		available_cash: '$13,465,679.34',
 		today_change: '0.00% /$0.00',
 		positive: null
 	},
 	{
-		id: 'AAA',
+		identity: 4,
 		name: 'AAA - 3810',
 		available_cash: '$10,050,054.07',
 		today_change: '+0.08% /$8,916,69',
 		positive: true
 	},
 	{
-		id: 'IRA',
+		identity: 5,
 		name: 'IRA - 5200',
 		available_cash: '$5,763.36',
 		today_change: '-0.08% /$8,916,69',
@@ -55,43 +56,27 @@ const ACCOUNTS_DATA: AccountsTypes[] = [
 
 	
 ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+
 title = 'morgan-stanley';
 hideAccountId = true;
 displayedColumns: string[] = ['name',  'available_cash'];
-dataSource = ACCOUNTS_DATA;
+dataSource = new MatTableDataSource(ACCOUNTS_DATA);
 
-sortedData: AccountsTypes[];
+@ViewChild(MatSort) sort: MatSort;
 
-
-constructor() {
- this.sortedData = this.dataSource.slice();
-}
-
-
- sortData(sort: Sort) {
-    const data = this.dataSource.slice();
-    if (!sort.active || sort.direction === '') {
-      this.sortedData = data;
-      return;
-    }
-
- this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'id': return this.compare(a.name, b.name, isAsc);
-        default: return 0;
-      }
-    });
-  }
 
 
  setColor(change) {
+ 	console.log('setColor()')
  	switch (change) {
  		case true :
  			return 'green';
@@ -102,11 +87,8 @@ constructor() {
  	}
  }
 
-
- compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-}
-
-
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
 
 }	
