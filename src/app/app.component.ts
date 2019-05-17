38ea0de11363c4f303ddd39d03e04db33bd07994
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-17 14:37:28 -0400

    created a pagination now I need to make the button disappear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,14 @@ const ACCOUNTS_DATA: AccountsTypes[] = [
 		today_change: '0.21% / $38,881.63',
 		positive: true
 	},
-	{
+	
+
+	
+];
+
+
+const secondPush: AccountsTypes[] = [
+{
 		identity: '2019',
 		name: 'REG',
 		available_cash: '13,465,679.34',
@@ -57,12 +64,35 @@ const ACCOUNTS_DATA: AccountsTypes[] = [
 		name: 'IRA',
 		available_cash: '5,763.36',
 		cash: 57630,
-		today_change: '0.08% / $8,916,69',
+		today_change: '0.07% / $8,916,69',
 		positive: false
-	}
+	},
+		{
+		identity: '0029',
+		name: 'AAA',
+		available_cash: '39,160,334.42',
+		cash: 39160334,
+		today_change: '0.07% / $31, 435,87',
+		positive: false
+	},
+	{
+		identity: '0146' ,
+		name: 'IRA',
+		available_cash: '15,884,302.39',
+		cash: 15884302,
+		today_change: '0.03% / $7,430.83',
+		positive: true
+	},
+	{
+		identity: '1812' ,
+		name: 'AAA',
+		available_cash: '2,010,926.10',
+		cash: 2010926,
+		today_change: '0.21% / $38,881.63',
+		positive: true
+	},
+	];
 
-	
-];
 
 @Component({
   selector: 'app-root',
@@ -70,7 +100,6 @@ const ACCOUNTS_DATA: AccountsTypes[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
 
 
 title = 'morgan-stanley';
@@ -102,6 +131,11 @@ setStatus(check: boolean){
  		case null :
  			return 'gray';	
  	}
+ }
+
+ loadMoreAccounts() {
+ 	console.log(' Clicking the loadMoreAccounts Button');
+     this.dataSource = new MatTableDataSource(secondPush);
  }
 
   ngOnInit() {
